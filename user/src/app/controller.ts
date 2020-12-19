9a420ab4ec7e70845a428ebbf077ab7d027803a6
Giulia Peserico <giu.peserico@gmail.com>
2020-12-19 11:54:45 +0100

    Implementation getUserSettings

--- user/src/app/controller.ts
+++ user/src/app/controller.ts
@@ -12,12 +12,10 @@
 import { Request, Response } from 'express';
 import axios from 'axios';
 import { userOrm } from './orm';
-import jwt from 'jsonwebtoken';
 
 export const createUserSettings = async (req: Request, res: Response) => {
   const universitySlugRequest = req.body['universitySlug'];
   const token = req.body['token'];
-  console.log("--------------CALL1: "+token)
   try {
     const authCheck = await axios.post<{id: number}>('http://authentication/', {
         token: token
@@ -28,20 +26,20 @@ export const createUserSettings = async (req: Request, res: Response) => {
       if(universitySlug) {
         const user = await userOrm.updateUserSetting(authCheck.data.id, universitySlug);
         if(user){
-          res.sendStatus(201);
+          res.status(201);
           res.send(user);
         } else {
-          res.sendStatus(404);
+          res.status(404);
           res.send({ error: 'User not found'});
         }
       } else {
-        res.sendStatus(404)
+        res.status(404)
         res.send({
           error: 'University not found',
         });
       }
     } else {
-      res.sendStatus(404)
+      res.status(404)
       res.send({
         error: 'User not found',
       });
@@ -49,30 +47,29 @@ export const createUserSettings = async (req: Request, res: Response) => {
 
   } catch (e) {
     console.error(e)
-    res.sendStatus(500);
+    res.status(500);
     res.send({error: e.toString()});
   }
 };
 export const getUserSettings = async (req: Request, res: Response) => {
-  // const token = req.query['token'];
-  // console.log("---------"+token)
-  // try {
-  //   const authCheck = await axios.post<{ id: number }>('http://authentication/', {
-  //     token: token
-  //   })
-  //   if(authCheck.data.id) {
-  //     const user = await userDao.getUserById(authCheck.data.id);
-  //     if(user) {
-  //       res.sendStatus(200);
-  //       res.send(user);
-  //     } else {
-  //       res.sendStatus(404);
-  //       res.send({ error: 'User not found'});
-  //     }
-  //   }
-  // } catch (e) {
-  //   console.error(e)
-  //   res.sendStatus(500);
-  //   res.send({error: e.toString()});
-  // }
+  const token = req.query['token'];
+  try {
+    const authCheck = await axios.post<{ id: number }>('http://authentication/', {
+      token: token
+    })
+    if(authCheck.data.id) {
+      const user = await userOrm.getUserById(authCheck.data.id);
+      if(user) {
+        res.status(200);
+        res.send(user);
+      } else {
+        res.status(404);
+        res.send({ error: 'User not found'});
+      }
+    }
+  } catch (e) {
+    console.error(e)
+    res.status(500);
+    res.send({error: e.toString()});
+  }
 }
